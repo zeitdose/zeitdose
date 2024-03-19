bf254473b2260c02efd5c21286e1414b6bd90431
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-19 18:09:47 +0800

    chore(next): strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import MillionLint from '@million/lint'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true
+}
+
 export default MillionLint.next({
   rsc: true,
 })(nextConfig)
