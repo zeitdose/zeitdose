89313c1221e659da2aa75b7867455c8057525669
SevicheCC <91365763+Sevichecc@users.noreply.github.com>
2024-03-31 22:18:23 +0800

    chore: reset radix color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   darkMode: ['class'],
@@ -53,6 +54,34 @@ export default {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        slate: {
+          1: 'var(--slate-1)',
+          2: 'var(--slate-2)',
+          3: 'var(--slate-3)',
+          4: 'var(--slate-4)',
+          5: 'var(--slate-5)',
+          6: 'var(--slate-6)',
+          7: 'var(--slate-7)',
+          8: 'var(--slate-8)',
+          9: 'var(--slate-9)',
+          10: 'var(--slate-10)',
+          11: 'var(--slate-11)',
+          12: 'var(--slate-12)',
+        },
+        slateA: {
+          1: 'var(--slate-a1)',
+          2: 'var(--slate-a2)',
+          3: 'var(--slate-a3)',
+          4: 'var(--slate-a4)',
+          5: 'var(--slate-a5)',
+          6: 'var(--slate-a6)',
+          7: 'var(--slate-a7)',
+          8: 'var(--slate-a8)',
+          9: 'var(--slate-a9)',
+          10: 'var(--slate-a10)',
+          11: 'var(--slate-a11)',
+          12: 'var(--slate-a12)',
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
@@ -72,6 +101,9 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
